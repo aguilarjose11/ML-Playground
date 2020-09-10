24cbb28bc6a98b7518a0f9234467730b841985a5
Jose Efraim Aguilar Escamilla <aguilar.jose11@outlook.com>
2020-09-09 22:29:50 -0500

    add linear math functions.

--- ml-by-scratch/GradientDescent.py
+++ ml-by-scratch/GradientDescent.py
@@ -3,3 +3,31 @@ Gradient Descent is a technique used in machine learning to
 __optimize__ the problem of finding the best model for a 
 given dataset.
 '''
+from sympy import limit, symbols
+
+from typing import Callable
+
+
+# Regular Differencial quotient used for differenciation:
+'''
+| *
+| \*       Tangent line
+|   \*
+|     \ *
+|____________*_____       _____________________
+                         |       Quotient      |
+(d/dx)*f(x) = lim_(h->0) [f(x + h) - f(x)] / h   
+
+'''
+def difference_quotient(f: Callable[[float], float],
+                        x: float,
+                        h: float) -> float:
+    return (f(x + h) - f(x))/h
+
+
+def differenciate(f: Callable[[float], float],
+                  x: float) -> float:
+    
+    h = symbols('h')
+    expr = (f(x + h) - f(x))/h
+    return limit(expr, h, 0)
--- ml-by-scratch/linear_algebra.py
+++ ml-by-scratch/linear_algebra.py
@@ -11,6 +11,7 @@ from typing import List, Tuple, Callable
 # Vectors store a single row of a matrix.
 Vector = List[float]
 
+
 # Vectorial Addition.
 def add(v: Vector, w: Vector) -> Vector:
     '''
@@ -24,7 +25,8 @@ def add(v: Vector, w: Vector) -> Vector:
     '''
     assert len(v) == len(w), f"Dimension of vectors do not match: dim(v)={len(v)} != dim(w)={len(w)}."
     return [v_x + w_x for v_x, w_x in zip(v, w)]
-    
+
+
 # Vectorial Subtraction.
 def subtract(v: Vector, w: Vector) -> Vector:
     '''
@@ -38,6 +40,7 @@ def subtract(v: Vector, w: Vector) -> Vector:
     '''
     assert len(v) == len(w), f"Dimension of vectors do not match: |v|={len(v)} != |w|={len(w)}."
     return [v_x - w_x for v_x, w_x in zip(v, w)]
+
 
 # Multiple vector addition.
 def vector_sum(vectors: List[Vector]) -> Vector:
@@ -53,9 +56,11 @@ def vector_sum(vectors: List[Vector]) -> Vector:
     return [sum(vector[x] for vector in vectors)
             for x in range(n_vector_dim)]
 
+
 # Scalar Product.
 def scalar_multiply(c: float, v: Vector) -> Vector:
     return [c * v_n for v_n in v]
+
 
 # Component-Wise vectorial mean.
 def vector_mean(vectors: List[Vector]) -> Vector:
@@ -64,48 +69,57 @@ def vector_mean(vectors: List[Vector]) -> Vector:
     assert all(len(v) == n_vector_dim for v in vectors), "dimensions do not match!"
     return scalar_multiply(1/len(vectors), vector_sum(vectors))
 
+
 # dot product.
 def dot(v: Vector, w: Vector) -> float:
     assert len(v) == len(w), "vector sizes are different!"
     return sum(v_n * w_n for v_n, w_n in zip(v, w))
 
+
 # Summation of squares.
 def sum_of_squares(v: Vector) -> float:
     return dot(v, v)
 
+
 # Vectorial Magnitude.
 def magnitude(v: Vector) -> float:
     return math.sqrt(sum_of_squares(v))
+
 
 # Vectorial distance.
 def distance(v: Vector, w: Vector) -> float:
     return magnitude(subtract(v, w))
 
 
-#----------------
+# ----------------
 # Matrix Algebra |
-#----------------
+# ----------------
 
 # Note: Matrix = List[Vector]
 Matrix = List[List[float]]
+
 
 # Matrix shape
 def shape(A: Matrix) -> Tuple[int, int]:
     return len(A), len(A[0])
 
+
 # Matrix row extraction
 def get_row(A: Matrix, i: int) -> Vector:
     return A[i]
 
+
 # Matrix column extraction
 def get_column(A: Matrix, j: int) -> Vector:
     return [column[j] for column in A]
+
 
 # Matrix generation with specified dimensions
 def make_matrix(num_rows: int,
                 num_cols: int,
                 entry_fn: Callable[[int, int], float]) -> Matrix:
     return [[entry_fn(row, col) for col in range(num_cols)] for row in range(num_rows)]
+
 
 # Identity Matrix generator.
 def identity_matrix(n: int) -> Matrix:
@@ -114,15 +128,17 @@ def identity_matrix(n: int) -> Matrix:
             return 1
         else:
             return 0
-    
     return make_matrix(n, n, identity_matrix_base_func)
+
 
 # Matrix scalar product
 def matrix_scalar_multiply(c: float, A: Matrix) -> Matrix:
     return [scalar_multiply(c, row) for row in A]
 
+
 def transpose(A: Matrix) -> Matrix:
     return [[row[col] for row in A] for col in range(len(A[0]))]
+
 
 # Matrix cross-product
 def matrix_dot(A: Matrix, B: Matrix) -> Matrix:
