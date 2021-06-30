Fixed backprop

ANN/ANN.py
@@ -89,7 +89,7 @@ class Network:
 
     
     def sigmoid_prime(self,
-        z: np.ndarray) -> float:
+        z: np.ndarray) -> Union[np.ndarray, float]:
         """Derivative of sigmoid function.
         
         This derivative is defined as follows:
@@ -172,20 +172,16 @@ class Network:
         is defined as follows:
 
          d MSE        d        1
-        -------- = -------- * --- ||y_pred - y||^2 = 
+        -------- = -------- * --- (y_pred - y)^2 = 
         d y_pred   d y_pred    2
 
                               d
-        = ||y_pred - y|| * -------- * ||y_pred - y|| = ||y_pred - y||
+        = (y_pred - y) * -------- * (y_pred - y) = (y_pred - y)
                            d y_pred
         """
 
-        # len(shape) == 1 represents an array
-        if type(y) == type(y_pred) == float or len(y_pred.shape) == 1:
-            # Single value passed
-            return np.linalg.norm(y_pred - y)
-        else:
-            return [np.linalg.norm(a - yy) for a, yy in zip(y_pred, y)]
+        return y_pred - y
+
 
 
 
@@ -196,12 +192,15 @@ class Network:
         Feed forward information. Will return the
         activation for every layer.
         
+        Input must be (n, 1) shape. 
+        
         parameters
         ----------
         a: np.ndarray
             Input data
         """
 
+        assert len(a.shape) == 2, f"Invalid shape {a.shape}."
         output = [a.copy()]
         for b_l, w_l in zip(self.biases, self.weights):
             z_l = np.matmul(w_l, a) + b_l
@@ -211,15 +210,72 @@ class Network:
         return output
 
     
+    def _feedforward(self, 
+        a: np.ndarray) -> List[np.ndarray]:
+        """Network solve function
+        
+        Feed forward information. Will return the activation for 
+        every layer as well as its weighted sum. This function is 
+        for exclusive use in the backpropagation algorithm
+        
+        Input must be (n, 1) shape. 
+        
+        parameters
+        ----------
+        a: np.ndarray
+            Input data
+        """
+
+        assert len(a.shape) == 2, f"Invalid shape {a.shape}."
+        output = [a.copy()]
+        z = []
+        for b_l, w_l in zip(self.biases, self.weights):
+            z_l = np.matmul(w_l, a) + b_l
+            a_l = self.sigmoid(z_l)
+            z.append(z_l)
+            output.append(a_l)
+            a = a_l # feed to next layer
+        return output, z
+
+
+
+    def backprop(self,
+        X: List[Tuple[float, ...]],
+        y: List[float],
+        epochs: int=50,
+        eta: float=1.,
+        X_test: List[Tuple[float, ...]]=None,
+        y_test: List[float]=None,
+        sgd: bool=True):
+
+        for epoch in range(epochs):
+            for datum, label in zip(X, y):
+                a, z = self._feedforward(datum)
+                # Backpropagate through output layer
+                
+                del_k = self.sigmoid(z[-1])*(1-self.sigmoid(z[-1]))*(label - self.sigmoid(z[-1]))
+                self.weights[-1] += eta * np.matmul(del_k, a[-2].T)
+                self.biases[-1] += eta * del_k
+
+                for l in range(len(self.biases)-2, -1, -1):
+                    del_h = self.sigmoid(z[l])*(1-self.sigmoid(z[l]))*np.matmul(self.weights[l+1].transpose(), del_k)
+                    self.weights[l] += eta * np.matmul(del_h, a[l].T)
+                    self.biases[l] += eta * del_h
+                    del_k = del_h
+
+            print(f"Epoch {epoch} completed.")
+
+
+
     def train(self,
         X: List[Tuple[float, ...]],
         y: List[float],
         epochs: int=50,
-        eta: float=0.01,
+        eta: float=1.,
         X_test: List[Tuple[float, ...]]=None,
         y_test: List[float]=None,
         sgd: bool=True,
-        mini_batch_size: int=1):
+        mini_batch_size: int=1) -> None:
         """ ANN training
 
         This ANN will utilize backpropagation. Both stochastic and
@@ -265,44 +321,58 @@ class Network:
                 del_b = [np.zeros(layer.shape) for layer in self.biases]
                 del_w = [np.zeros(layer.shape) for layer in self.weights]
 
-                for X_t, y_t in zip(X, y):
-                    # Calculate Output Layer
-                    a = self.feedforward(X_t)
-                    z_L = np.matmul(self.weights[-1], a[-2]) + self.biases[-1]
-                    # numpy's array's * product is the hadamard product by default
-                    # Output layer's error
-                    delta_L = self.mse_prime(a, y_t) * self.sigmoid_prime(z_L)
-                    # Add output changes.
-                    del_b_l = [delta_L]
-                    del_w_l = [a[-2] * delta_L]
+                for datum, target in zip(X, y):
+                    del_b_t = []
+                    del_w_t = []
+                    # Forwardpropagate
+                    a, z = self._feedforward(datum)
+                    # Output cost gradient, conserves dimensions of output
+                    grad_C_L = self.mse_prime(a[-1], target)
+                    # output Net (weighted sum)
+                    z_L = z[-1]
+                    # sigma derivative, conserves dimensions of z
+                    sigma_L  = self.sigmoid_prime(z_L)
+                    # Double check for errors
+                    assert grad_C_L.shape == sigma_L.shape, "Problem with hadamard product!"
+                    # find output layer error. Hadamard product equivalent.
+                    del_L = grad_C_L * sigma_L
+                    # Find output bias changes
+                    del_b_t.append(del_L)
+                    # Find output weights changes.
+                    # Dimensions shall be same as weight matrix of layer.
+                    del_w_t.append(np.matmul(del_L, a[-2].T))
+                    # Prepare for backpropagation
+                    del_l = del_L
 
-                    # Calculate hidden layers
-                    delta_l = delta_L
-
-                    for l in range(len(self.weights[-2::-1]), 0, -1):
-                        z_l     = np.matmul(self.weights[l], a[l-1]) + self.biases[l]
-                        delta_l = (self.weights[l].transpose()*delta_l) * self.sigmoid_prime(z_l)
-                        del_b_l.append(delta_l)
-                        del_w_l.append(a[l-1] * delta_l)
-
-                    # To save time, changes were appended to the end.
-                    # Thus, Why we need to reverse them so to match.
-                    del_b_l.reverse()
-                    del_w_l.reverse()
-                    for l, b, w in range(zip(del_b_l, del_w_l)):
+                    # For every layer...
+                    for l in range(len(self.weights[:-2]), -1, -1):
+                        # Because of feedworward function, x is included,
+                        # Thus shifting activations to the right and making
+                        # a^l-1 => a^l
+                        z_l = z[l]
+                        del_l = np.matmul(self.weights[l+1].T, del_l) * self.sigmoid_prime(z_l)
+                        del_b_t.append(del_l)
+                        del_w_t.append(np.matmul(del_l, a[l].T))
+                    
+                    # Add gradients for current training instance.
+                    del_b_t.reverse()
+                    del_w_t.reverse()
+                    for l, (b, w) in enumerate(zip(del_b_t, del_w_t)):
                         del_b[l] += b
-                        del_w[l] += w
+                        del_w[l] += w # Do numpy's matrices add as expected?
+                
+                #print(f"before: Bias: {self.biases} Weights: {self.weights}")
+                for l, (b, w) in enumerate(zip(del_b, del_w)):
+                    # gradients are averaged out. Optional tho.
+                    self.biases[l]  += (b / len(y)) * eta
+                    self.weights[l] += (w / len(y)) * eta
+                #print(f"after: Bias: {self.biases} Weights: {self.weights}")
 
-                # Calculate averaged modification.
-                for l, _, _ in range(zip(del_b, del_w)):
-                    del_b[l] /= len(y)
-                    del_w[l] /= len(y)
+                print(f"Epoch {epoch} completed")
+                
 
-                # Apply step
-                for l, b, w, _, _ in range(zip(del_b, del_w, self.weights, self.biases)):
-                    self.biases[l]  += eta * b
-                    self.weights[l] += eta * w
 
+              
     def gradient_descent(self,
         data: List[Any],
         eta: float):